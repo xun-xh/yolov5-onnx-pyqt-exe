76042983016ad2dd0563661406014b382fc7a1a1
xunxun-surface <xihuihai@gmail.com>
2022-11-18 15:51:25 +0800

    删除发布文件

--- build_file/publish/cv2/version.py
+++
@@ -1,4 +0,0 @@
-opencv_version = "4.5.4.60"
-contrib = False
-headless = False
-ci_build = True
--- build_file/publish/onnxruntime/quantization/__init__.py
+++
@@ -1,5 +0,0 @@
-from .quantize import quantize, quantize_static, quantize_dynamic
-from .quantize import QuantizationMode
-from .calibrate import CalibrationDataReader, CalibraterBase, MinMaxCalibrater, create_calibrator, CalibrationMethod
-from .quant_utils import QuantType, QuantFormat, write_calibration_table
-from .qdq_quantizer import QDQQuantizer
